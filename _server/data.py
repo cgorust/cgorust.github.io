e881cf6f5247941fb661794808f6b7b9a3481abd
cgorust <cgorust@gmail.com>
2022-09-06 10:12:42 -0400

    remove pyc

--- _server/data.py
+++ _server/data.py
@@ -9,13 +9,15 @@ class Data(object, metaclass=Singleton):
     dictionary = {}
 
     def __init__(self):
-        self.wordPageTemplate = File().getHTML("_server/template/wordPage.html")
+        self.indexPageTemplate = File().getHTML("_server/template/page.html")
+        self.wordPageTemplate = copy.deepcopy(self.indexPageTemplate)
+        self.wordPageTemplate.find("main").append(File().getHTML("_server/template/word.html"))
+
         self.populateDictionary()
         print("data populated")
 
     def headerToPath(self, header):
         return header.replace(" ", "_").capitalize()
-
 
     def populateDictionary(self):
         apply = True
@@ -23,21 +25,17 @@ class Data(object, metaclass=Singleton):
             file, header, content, superConcepts, superCategories, subConcepts, subCategories = self.getWord(path)
             self.dictionary[header] = {}
             if self.headerToPath(header) != path.replace("dictionary/", "").replace(".html", ""):
-                print("Path and header not match: " + path + ", " + header)
+                print("Path and header is not match. Path: " + path + "; header: " + header)
                 #os.replace(path, "dictionary/"+ self.headerToPath(header) + ".html")
-            self.dictionary[header]["content"]=content
-            if superConcepts != None:
-                self.dictionary[header]["superConcepts"]=superConcepts
-            if superCategories != None:
-                self.dictionary[header]["superCategories"]=superCategories
-            if subConcepts != None:
-                self.dictionary[header]["subConcepts"]=subConcepts
-            if subCategories != None:
-                self.dictionary[header]["subCategories"]=subCategories
+            self.dictionary[header]["Content"]=content
+            self.dictionary[header]["Superconcept"]=superConcepts
+            self.dictionary[header]["Supercategory"]=superCategories
+            self.dictionary[header]["Subconcept"]=subConcepts
+            self.dictionary[header]["Subcategory"]=subCategories
             if apply:
                 newFile = self.applyNewTemplate(header)
                 if str(file) == str(newFile):
-                    print("template has no change")
+                    print("Template did no change")
                     apply = False
                 else:
                     f = open(path, "w")
@@ -48,73 +46,39 @@ class Data(object, metaclass=Singleton):
     def getAddr(self, header):
         return BeautifulSoup("<a href=\"/dictionary/" + self.headerToPath(header) + "\">"+ header +"</a>", 'html.parser')
 
+    def setRelation(self, file, relationName, header):
+        relations = File().getRelationNode(file, relationName+ ": ").parent
+        #print(self.dictionary[header])
+        if self.dictionary[header][relationName] != []:
+            first = True
+            for r in self.dictionary[header][relationName]:
+                if first:
+                    first = False
+                else:
+                    relations.append(", ")    
+                relations.append(self.getAddr(r)) 
+        else:
+            relations.attrs["hidden"]=None  
+
     def applyNewTemplate(self, header):
         file = copy.deepcopy(self.wordPageTemplate)
-        file.find("div", {"id": "dict_container"}).find("h1").find("span").string=header
-        file.find("pre", {"id": "word_content"}).string=self.dictionary[header]["content"]
-        superConcepts = file.find("b", text="Superconcept: ").parent
-        if self.dictionary[header]["superConcepts"] != []:
-            first = True
-            for x in self.dictionary[header]["superConcepts"]:
-                if first:
-                    first = False
-                else:
-                    superConcepts.append(", ")    
-                superConcepts.append(self.getAddr(x)) 
-        else:
-            superConcepts.attrs["hidden"]=None  
-        superCategories = file.find("b", text="Supercategory: ").parent
-        if self.dictionary[header]["superCategories"] != []:
-            first = True
-            for x in self.dictionary[header]["superCategories"]:
-                if first:
-                    first = False
-                else:
-                    superCategories.append(", ")    
-                superCategories.append(self.getAddr(x)) 
-        else:
-            superCategories.attrs["hidden"]=None  
-        subConcepts = file.find("b", text="Subconcept: ").parent
-        if self.dictionary[header]["subConcepts"] != []:
-            first = True
-            for x in self.dictionary[header]["subConcepts"]:
-                if first:
-                    first = False
-                else:
-                    subConcepts.append(", ")    
-                subConcepts.append(self.getAddr(x)) 
-        else:
-            subConcepts.attrs["hidden"]=None  
-        subCategories = file.find("b", text="Subcategory: ").parent
-        if self.dictionary[header]["subCategories"] != []:
-            first = True
-            for x in self.dictionary[header]["subCategories"]:
-                if first:
-                    first = False
-                else:
-                    subCategories.append(", ")    
-                subCategories.append(self.getAddr(x)) 
-        else:
-            subCategories.attrs["hidden"]=None  
+        File().getHeaderNode(file).string=header
+        File().getContentNode(file).string=self.dictionary[header]["Content"]
+        self.setRelation(file, "Superconcept", header)
+        self.setRelation(file, "Supercategory", header)
+        self.setRelation(file, "Subconcept", header)
+        self.setRelation(file, "Subcategory", header)
         return file
-
-    def getRelations(self, file, relationName):
-        relations = file.find("b", text=relationName)
-        if relations!= None:
-            relations=[x.getText() for x in relations.findNextSiblings("a", recursive=False)]
-        else:
-            relations=[]
-        return relations
 
     def getWord(self, path):
         file = File().getHTML(path)
         #print(path)
-        header = file.find("div", {"id": "dict_container"}).find("h1").find("span").getText()
-        content = file.find("pre", {"id": "word_content"}).getText()
-        superConcepts = self.getRelations(file, "Superconcept: ")
-        superCategories = self.getRelations(file, "Supercategory: ")
-        subConcepts = self.getRelations(file, "Subconcept: ")
-        subCategories = self.getRelations(file, "Subcategory: ")
+        header = File().getHeaderNode(file).getText()
+        content = File().getContentNode(file).getText()
+        superConcepts = File().getRelations(file, "Superconcept: ")
+        superCategories = File().getRelations(file, "Supercategory: ")
+        subConcepts = File().getRelations(file, "Subconcept: ")
+        subCategories = File().getRelations(file, "Subcategory: ")
         return [file, header, content, superConcepts, superCategories, subConcepts, subCategories]
 
     def checkWordPage(self, path):
@@ -125,11 +89,11 @@ class Data(object, metaclass=Singleton):
         if header not in self.dictionary:
             print("page is not in cache")
             return
-        if self.dictionary[header]["content"] != content or \
-            self.dictionary[header]["superConcepts"] != superConcepts  or \
-            self.dictionary[header]["superCategories"] != superCategories  or \
-            self.dictionary[header]["subConcepts"] != subConcepts  or \
-            self.dictionary[header]["subCategories"] != subCategories:
+        if self.dictionary[header]["Content"] != content or \
+            self.dictionary[header]["Superconcept"] != superConcepts  or \
+            self.dictionary[header]["Supercategory"] != superCategories  or \
+            self.dictionary[header]["Subconcept"] != subConcepts  or \
+            self.dictionary[header]["Subcategory"] != subCategories:
             print("page content is not the same as in cache")
             return 
 
--- _server/file.py
+++ _server/file.py
@@ -3,15 +3,26 @@ from singleton import Singleton
 from bs4 import BeautifulSoup
 import difflib
 
-class File(object):
-    __metaclass__ = Singleton    
-
+class File(object, metaclass=Singleton):
     def getHTML(self, path):
         with open(path) as fp:
             return BeautifulSoup(fp, 'html.parser')
 
-    def diffFile(self, pageA, pageB):
-        diffList = difflib.ndiff(pageA, pageB) 
-        if next(diffList, None) is not None:
-            print("file changed")
-           
+    def getHeaderNode(self, file):
+        return file.find("div", {"id": "dict_container"}).find("h1").find("span")           
+
+    def getContentNode(self, file):
+        return file.find("pre", {"id": "word_content"})          
+
+    def getRelationNode(self, file, relationName):
+        return file.find("b", text= relationName)         
+
+    def getRelations(self, file, relationName):
+        relations = File().getRelationNode(file, relationName)
+        if relations!= None:
+            relations=[x.getText() for x in relations.findNextSiblings("a", recursive=False)]
+        else:
+            relations=[]
+        return relations
+
+      
